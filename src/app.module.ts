789efb3dd57bc8411e55796bf41872029c688885
Nguyễn Văn Thái <123443192+iaht211@users.noreply.github.com>
2024-04-18 22:47:01 +0700

    add room module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,12 @@ import { ConfigModule } from '@nestjs/config';
 import { PackagesModule } from './packages/packages.module';
 import { FeedbacksModule } from './feedbacks/feedbacks.module';
 import { StaffModule } from './staff/staff.module';
+import { RoomModule } from './room/room.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), PrismaModule, UsersModule, AuthModule, PackagesModule, FeedbacksModule, StaffModule],
+  }), PrismaModule, UsersModule, AuthModule, PackagesModule, FeedbacksModule, StaffModule, RoomModule],
   controllers: [AppController],
   providers: [AppService],
 })
